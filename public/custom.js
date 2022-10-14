e2d2a8d4674359a146266f2afcc8702da43dacc6
emon21 <emonbd66@gmail.com>
2022-10-14 17:59:00 +0600

    testing

--- public/custom.js
+++ public/custom.js
@@ -1,6 +1,16 @@
 /* Custom JS code Start */
 
+ //blog slider
 
+ $('.single-item').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: true
+    
+  });
 
 
 /* Custom JS code End */
