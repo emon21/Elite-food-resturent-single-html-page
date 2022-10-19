2147b9a820e7c8e9fbac74da96e9038127fe2061
emon21 <emonbd66@gmail.com>
2022-10-19 17:52:34 +0600

    test

--- public/custom.js
+++ public/custom.js
@@ -101,3 +101,40 @@ $(window).on("scroll", function () {
 //       }
 //     })
 //   })
+
+
+// create an array of objects with the id, trigger element (eg. button), and the content element
+const tabElements = [
+    {
+        id: 'profile',
+        triggerEl: document.querySelector('#profile-tab-example'),
+        targetEl: document.querySelector('#profile-example')
+    },
+    {
+        id: 'dashboard',
+        triggerEl: document.querySelector('#dashboard-tab-example'),
+        targetEl: document.querySelector('#dashboard-example')
+    },
+    {
+        id: 'settings',
+        triggerEl: document.querySelector('#settings-tab-example'),
+        targetEl: document.querySelector('#settings-example')
+    },
+    {
+        id: 'contacts',
+        triggerEl: document.querySelector('#contacts-tab-example'),
+        targetEl: document.querySelector('#contacts-example')
+    }
+];
+
+// options with default values
+const options = {
+    defaultTabId: 'profile',
+    activeClasses: 'text-green-600 hover:text-green-600 dark:hover:text-blue-400 border-green-600',
+    inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
+    onShow: () => {
+        console.log('tab is shown');
+    }
+};
+
+const tabs = new Tabs(tabElements, options);
